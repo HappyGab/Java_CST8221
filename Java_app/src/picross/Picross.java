359b5bccfdae8dada7dff7ca9b239f14e9b14545
HappyGab <105558236+HappyGab@users.noreply.github.com>
2023-01-31 19:41:00 -0500

    Update Picross.java

--- Java_app/src/picross/Picross.java
+++ Java_app/src/picross/Picross.java
@@ -1,12 +1,26 @@
 package picross;
 
 import javax.swing.*;
+import java.awt.Color;
 
 public class Picross extends JFrame{
 
 	public static void main(String[] args) {
 
-
+		JFrame game = new JFrame();
+		
+		game.setSize(750,575);
+		
+		
+		JLabel topLeft = new JLabel("top left");
+		topLeft.setBounds(0,0,150,150);
+		topLeft.setBackground(Color.RED);
+		topLeft.setOpaque(true);
+		
+		game.add(topLeft);
+		
+		game.setLayout(null);
+		game.setVisible(true);
 		
 
 	}

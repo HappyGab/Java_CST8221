6bed1b347c0d67d5c75e52b1dcd3ce011500c357
Sistemee <dagedar123@gmail.com>
2023-01-31 20:08:45 -0500

    Update Picross.java

--- Java_app/src/picross/Picross.java
+++ Java_app/src/picross/Picross.java
@@ -1,6 +1,7 @@
 package picross;
 
 import javax.swing.*;
+
 import java.awt.Color;
 
 public class Picross extends JFrame{
@@ -13,6 +14,7 @@ public class Picross extends JFrame{
 		
 		
 		JLabel topLeft = new JLabel("top left");
+	 
 		topLeft.setBounds(0,0,150,150);
 		topLeft.setBackground(Color.RED);
 		topLeft.setOpaque(true);
@@ -22,8 +24,42 @@ public class Picross extends JFrame{
 		game.setLayout(null);
 		game.setVisible(true);
 		
-
+		JLabel topRight = new JLabel("top right");
+		JLabel top = new JLabel("top ");
+		JLabel right = new JLabel("right");
+		JLabel mid = new JLabel("middle");
+		JLabel left = new JLabel("left");
+		JLabel bottomRight = new JLabel("timer");
 		
+		topRight.setBounds(585, 0, 150, 150);
+		topRight.setBackground(Color.blue);
+		topRight.setOpaque(true);
+		game.add(topRight);
+
+		top.setBounds(150, 0, 580, 150);
+		top.setBackground(Color.GRAY);
+		top.setOpaque(true);
+		game.add(top);
+		
+		right.setBounds(0, 150, 150, 600);
+		right.setBackground(Color.green);
+		right.setOpaque(true);
+		game.add(right);
+		
+		left.setBounds(585, 150, 150, 600);
+		left.setBackground(Color.CYAN);
+		left.setOpaque(true);
+		game.add(left);
+		
+		mid.setBounds(150, 150, 600, 600);
+		mid.setBackground(Color.yellow);
+		mid.setOpaque(true);
+		game.add(mid);
+		
+		bottomRight.setBounds(585, 500, 150, 100);
+		bottomRight.setBackground(Color.orange);
+		bottomRight.setOpaque(true);
+		game.add(bottomRight);
 	}
 
 }

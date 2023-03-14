53412ab9226473f4db7caad709ef744d1da3d2ab
HappyGab <105558236+HappyGab@users.noreply.github.com>
2023-03-14 02:19:01 -0400

    timer stuff in controller

--- Java_app/src/picross/GameController.java
+++ Java_app/src/picross/GameController.java
@@ -2,6 +2,7 @@ package picross;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.text.DecimalFormat;
 
 import javax.swing.JButton;
 
@@ -66,5 +67,27 @@ public class GameController  {
 			resetGame();
 			gameView.updateComponents();
 		}		
+	}
+	
+	class TimerListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			DecimalFormat format = new DecimalFormat("00"); 
+			String dSecond; 
+			String dMinute; 
+			
+			gameView.second ++;
+			
+			if (gameView.second==60) {
+				gameView.second = 0;
+				gameView.minutes++;
+			}
+			
+			dSecond = format.format(gameView.second); 
+			dMinute = format.format(gameView.minutes);
+			gameView.timerLabel.setText("" + dMinute +":" + dSecond);
+		}	
 	}
 }

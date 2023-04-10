76396edc6c6d57d785c6fc3223f7ce27881b9855
HappyGab <105558236+HappyGab@users.noreply.github.com>
2023-04-09 21:30:00 -0400

    Update clientMenuView.java

--- Java_app/src/picross/clientMenuView.java
+++ Java_app/src/picross/clientMenuView.java
@@ -15,6 +15,12 @@ public class clientMenuView extends JFrame{
 	private static JTextArea serverIP = new JTextArea(1, 0);
 	private static JTextArea portNum = new JTextArea(1, 0);
 	
+	private static JButton newGameButton = new JButton("New Game");
+	private static JButton sendGameButton = new JButton("Send Game");
+	private static JButton getGameButton = new JButton("Receive Game");
+	private static JButton sendDataButton = new JButton("Send Data");
+	private static JButton playButton = new JButton("Play");
+	
 	private static JTextArea controlPanel = new JTextArea();
 	
 	public void createMenu() {
@@ -22,7 +28,7 @@ public class clientMenuView extends JFrame{
 		this.setSize(750,300);
 		this.setResizable(false);
 		this.setTitle("Server Options");
-		this.setLayout(new GridLayout(3,1,10,10));
+		this.setLayout(new BorderLayout(10,10));
 		
 		JPanel northPanel = new JPanel();
 		northPanel.setLayout(new GridLayout(1,8,10,10));
@@ -39,16 +45,24 @@ public class clientMenuView extends JFrame{
 		northPanel.add(portNum);
 		northPanel.add(connectButton);
 		northPanel.add(disconnectButton);
+		northPanel.setPreferredSize(new Dimension(750,45));
 		
 		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(1,5,10,10));
+		centerPanel.add(newGameButton);
+		centerPanel.add(sendGameButton);
+		centerPanel.add(getGameButton);
+		centerPanel.add(sendDataButton);
+		centerPanel.add(playButton);
 		
 		controlPanel.setEditable(false);
         JScrollPane controlPanelScrollBar = new JScrollPane(controlPanel);
-        controlPanelScrollBar.setPreferredSize(new Dimension(750,200));
+        controlPanelScrollBar.setPreferredSize(new Dimension(750,150));
         controlPanelScrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
-		this.add(northPanel);
-		this.add(controlPanelScrollBar);
+		this.add(northPanel, BorderLayout.NORTH);
+		this.add(centerPanel, BorderLayout.CENTER);
+		this.add(controlPanelScrollBar, BorderLayout.SOUTH);
 		this.setVisible(true);
 	}
 	
@@ -86,5 +100,30 @@ public class clientMenuView extends JFrame{
 	public void addDisonnectListener(ActionListener listenButton) {
 		
 		disconnectButton.addActionListener(listenButton);
+	}
+		
+	public void addNewGameListener(ActionListener listenButton) {
+			
+		newGameButton.addActionListener(listenButton);
+	}
+	
+	public void addSendGameListener(ActionListener listenButton) {
+		
+		sendGameButton.addActionListener(listenButton);
+	}
+	
+	public void addGetGameListener(ActionListener listenButton) {
+		
+		getGameButton.addActionListener(listenButton);
+	}
+	
+	public void addSendDataListener(ActionListener listenButton) {
+		
+		sendDataButton.addActionListener(listenButton);
+	}
+	
+	public void addPlayListener(ActionListener listenButton) {
+		
+		playButton.addActionListener(listenButton);
 	}
 }
